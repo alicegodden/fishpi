8bf7d338178bfe600cfdb8f08d8344d33a6451b3
Dr. Alice Godden <136358959+alicegodden@users.noreply.github.com>
2024-10-24 09:51:51 +0100

    Update FishPi.py
    Signed-off-by: Dr. Alice Godden <136358959+alicegodden@users.noreply.github.com>

--- files/FishPi.py
+++ files/FishPi.py
@@ -683,3 +683,6 @@ analyse_uploaded_button.pack()
 analyse_uploaded_button.place(x=200, y=440, width=200)
 
 app.mainloop()
+
+# Add this at the end of your main GUI script
+app.after(15000, app.destroy)  # Closes the GUI after 15 seconds
